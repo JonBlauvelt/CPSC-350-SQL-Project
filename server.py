f878ae408b8058f3e811edbc87759d24011c19e8
jonblauvelt <juanold@hotmail.com>
2016-03-23 15:30:38 +0000

    moved folders to make flask things work.

--- server.py
+++ server.py
@@ -17,10 +17,10 @@ socketio = SocketIO(app)
 @app.route('/')
 def mainIndex():
     print 'in hello world'
-    return render_template('../index.html', methods=['GET','POST'])
+    return render_template('index.html', methods=['GET','POST'])
     
 
 
 # start the server - change to socketio
 if __name__ == '__main__':
-        socketio.run(app, host=os.getenv('IP', '0.0.0.0'), port =int(os.getenv('PORT', 8080)), debug=True)
+        app.run(host=os.getenv('IP', '0.0.0.0'), port =int(os.getenv('PORT', 9080)), debug=True)
